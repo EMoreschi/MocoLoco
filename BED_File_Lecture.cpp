d9cc3f36b20799885fe070f0243f51f25c7a6dee
EMoreschi <77453569+EMoreschi@users.noreply.github.com>
2021-01-19 15:39:00 +0100

    Update BED_File_Lecture.cpp

--- BED_File_Lecture.cpp
+++ BED_File_Lecture.cpp
@@ -7,7 +7,7 @@
 #include <sstream>
 
 
-class genomic_position { //creazione classe pubblica di tipo genomic position          
+class genomic_position { //creation public class of genomic_position type        
 	        public:
 		std::string chr;
 		int start;
@@ -15,30 +15,30 @@ class genomic_position { //creazione classe pubblica di tipo genomic position
 };
 
 int main(){
-	std::ifstream myfile ("imp_file.txt"); //apro il file in lettura// non deve essere hard-coded!!!!
-	std::vector<genomic_position> GEP; //definisco vettore
-	std::string chr; //definisco stringa
-        unsigned int start;  // definisco in start
-        unsigned int end;   // definisco end	
-	std::string line; //definisco line
+	std::ifstream myfile ("imp_file.txt"); //open file in lecture mode// it must not to be hard-coded!!!!
+	std::vector<genomic_position> GEP; // defining vector
+	std::string chr; //defining string
+        unsigned int start;  // defining start coordinate variable
+        unsigned int end;   // defining end coordinate variable
+	std::string line; //defining line
  
-        while(std::getline(myfile,line)){  //leggo il file in input riga per riga con funzione getline
-  	     if (line.empty())		   //se linea vuota passo alla successiva
+        while(std::getline(myfile,line)){  //reading input file line by line with getline function
+  	     if (line.empty())		   //CONTROL: if line is empty pass to next line
 		     continue;
-	      if (line[0] == '#')	//se linea inizia con # (possibili header o commenti) passo alla linea successiva
+	      if (line[0] == '#')	//CONTROL: if line start with # (possible headers or comments) pass to next line
 		     continue;
 
-              std::istringstream my_stream(line); //funzione istringstream per serparare, per ogni linea, parola per parola 	
+              std::istringstream my_stream(line); //istringstream function to separe each lines word by word	
 	      my_stream	>> chr >> start >> end;  
-	      GEP.push_back(genomic_position{chr, start, end}); //Alloco nel vettore GEP uno spazio per inserire oggetti di classe genomic_position
+	      GEP.push_back(genomic_position{chr, start, end}); //allocating in GEP vector a space to put in object of genomic_position class
 
 			}
 
-//	while(myfile >> chr >> start >> end) {   // fino a quando leggo gli oggetti
+//	while(myfile >> chr >> start >> end) {   // until it reads object
 //	       GEP.push_back(genomic_position{chr,start,end});	
 //	}
-	for (int i=0; i<GEP.size(); ++i)    // da 0 fino a tutta la lunghezza del vettore GEP
-	       std::cout<< GEP[i].end <<" "<< GEP[i].start << " " << GEP[i].chr << "\n"; //stampa per controllo 
+	for (int i=0; i<GEP.size(); ++i)    // from 0 to GEP vector length
+	       std::cout<< GEP[i].end <<" "<< GEP[i].start << " " << GEP[i].chr << "\n"; //control print 
 	return 0;
 }
 
