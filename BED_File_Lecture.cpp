4351ac053d7687800b190b11d804482205d670c2
EMoreschi <enrico.moreschi@studenti.unimi.it>
2021-01-20 14:53:06 +0100

    Added control if start coordinates are < then end coordinates

--- BED_File_Lecture.cpp
+++ BED_File_Lecture.cpp
@@ -11,29 +11,36 @@ using namespace std;
 class genomic_position { //creation public class of genomic_position type        
 	        
 		private:	//definition private field
+				
 		string chr;
 		unsigned int start;
 		unsigned int end;
+		bool flag;
+		
 		
 
 		public:			//definition public field
+		
+
+		//CONSTRUCTORS:
 		genomic_position(){	//default constructor
 		
 		chr = "";
 		start = 0;
 		end = 0;
-		
-			
+		flag = 0;	
 		} 
 
 		genomic_position(string chr_coord, unsigned int start_coord, unsigned int end_coord) {  //constructor given member values
-
+			
+			
 			chr = chr_coord;
 			start = start_coord;
 			end = end_coord;
-		
+			flag = flag_control(start_coord, end_coord);
 		}
 
+		//FUNCTIONS:
 		string get_char(){	//print char function
 		return chr;
 		}
@@ -45,18 +52,31 @@ class genomic_position { //creation public class of genomic_position type
 		int get_end(){		//print end coordinate funcion
 		return end;
 		}
+		
+		bool flag_control(unsigned int st, unsigned int en){ //function which controls that start coordinates are < then end coordinates
+
+		if(st > en){		//if start coordinates are > then end coordinates, flag is setted to 0
+			return 0;
+		}
+		else{ return 1;}
+		}	
+
+		bool get_flag(){		//print end coordinate funcion
+		return flag;
+		}
 };
 
 
 
 int main(){
-	ifstream myfile ("NCBI Homo sapiens Updated Annotation Release 109.20201120 on GRCh38.p13.BED"); //Opening file in lecture mode// it must not be hard-coded!!!!
+	ifstream myfile ("BED_di_prova.BED"); //Opening file in lecture mode// it must not be hard-coded!!!!
 	vector<genomic_position> GEP;	 //defining vector of genomic_position datas
 	vector<string> x;		//defining string vector x	
 	string line; 			//defining line string
 	string token;			//defining token string
-	genomic_position prova();	//initialization of class prova of genomic_position type using the default constructor
- 	
+	genomic_position prova();	//initialization of class prova of genomic_position type using the default constructor 	
+
+	int n_line = 0;			//line counter initialization
         while(getline(myfile,line)){  //reading input file line by line with getline function
   	     if (line.empty())		   //CONTROL: if line is empty pass to next line
 		     continue;
@@ -74,13 +94,21 @@ int main(){
 		genomic_position prova(x[0],s,e);  //modifiyng genomic_position class prova (inizialized as default before)
 						   //with the corrisponding data from the current file line
 		
-		GEP.push_back(genomic_position{prova});	//put the class prova in GAP (vector of classes of type genomic_position)
+		if(prova.get_flag() == 1){	//CONTROL: if flag is 1 means that the current line has starting coordinate > end coordinate, so it is correct
+
+			GEP.push_back(genomic_position{prova});	//put the class prova in GAP (vector of classes of type genomic_position)
+		}
+		else {		
+			cout << "WARNING: the line " << n_line << " is omitted because starting coordinates > end coordinates, please check your BED file!" << "\n";
+			//if flag is not 1 means that the current line has starting coordinate < end coordinate: PRINT WARNING!		
+		}
+
 		x.clear();				//Restore empty x vector
-		
+		n_line = n_line + 1;			//pass to next line 
 	}
 
 	for (int i=0; i<GEP.size(); ++i){    // from 0 to GEP vector length
-		cout<< GEP[i].get_char() << "\n" << GEP[i].get_start() << "\n" << GEP[i].get_end() << "\n\n"; //control print
+		cout<< GEP[i].get_char() << "\n" << GEP[i].get_start() << "\n" << GEP[i].get_end() << "\n" << GEP[i].get_flag() << "\n\n"; //control print
 		}
 
 }
