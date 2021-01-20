4aa6cce401d323cd972f3d79a6da27098de82a69
EMoreschi <enrico.moreschi@studenti.unimi.it>
2021-01-20 19:28:09 +0100

    Header file

---
+++ BED_File_Lecture.h.cpp
@@ -0,0 +1,70 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <typeinfo> 
+#include <sstream>
+
+using namespace std;
+
+class genomic_position { //creation public class of genomic_position type        
+	        
+		private:	//definition private field
+				
+		string chr;
+		unsigned int start;
+		unsigned int end;
+		bool flag;
+		
+		
+
+		public:			//definition public field
+		
+
+		//CONSTRUCTORS:
+		genomic_position(){	//default constructor
+		
+		chr = "";
+		start = 0;
+		end = 0;
+		flag = 0;	
+		} 
+
+		genomic_position(string chr_coord, unsigned int start_coord, unsigned int end_coord) {  //constructor given member values
+			
+			
+			chr = chr_coord;
+			start = start_coord;
+			end = end_coord;
+			flag = flag_control(start_coord, end_coord);
+		}
+
+		//FUNCTIONS:
+		string get_char(){	//print char function
+		return chr;
+		}
+
+		int get_start(){	//print start coordinate function
+		return start;
+		}
+
+		int get_end(){		//print end coordinate funcion
+		return end;
+		}
+		
+		bool flag_control(unsigned int st, unsigned int en){ //function which controls that start coordinates are < then end coordinates
+
+		if(st > en){		//if start coordinates are > then end coordinates, flag is setted to 0
+			return 0;
+		}
+		else{ return 1;}
+		}	
+
+		bool get_flag(){		//print end coordinate funcion
+		return flag;
+		}
+};
+
+
