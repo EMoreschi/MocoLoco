8ee4ef049be23dcbd07b7b2bde7492d9ada04c2a
Pietro <pietro.mandreoli@unimi.it>
2021-01-22 16:13:43 +0100

    code clean to test

--- BED_File_Lecture.cpp
+++ BED_File_Lecture.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]){
 	}
 
 	command_line_parser(argc, argv); //parser function called to handle aguments
-	
+
 	ifstream myfile (BED_FILE); //Opening file in lecture mode// it must not be hard-coded!!!!
 	vector<genomic_position> GEP;	 //defining vector of genomic_position datas
 	string line; 			//defining line string
@@ -16,33 +16,33 @@ int main(int argc, char *argv[]){
 	genomic_position new_class();	//initialization of class prova of genomic_position type using the default constructor 	
 
 	int n_line = 0;			//line counter initialization
-       
-	 while(getline(myfile,line)){  //reading input file line by line with getline function
 
-	 	vector<string> x;		//defining string vector x
+	while(getline(myfile,line)){  //reading input file line by line with getline function
 
-  	     	if (line.empty())		   //CONTROL: if line is empty pass to next line
-		     continue;
-	 	if (line[0] == '#')	//CONTROL: if line starts with # (possible headers or comments) pass to next line
-		     continue;
+		vector<string> x;		//defining string vector x
+
+		if (line.empty())		   //CONTROL: if line is empty pass to next line
+			continue;
+		if (line[0] == '#')	//CONTROL: if line starts with # (possible headers or comments) pass to next line
+			continue;
 
 		istringstream my_stream(line); //istringstream function to split each line word by word	
-	      
+
 		while(my_stream >> token){	//put every word in token string while words in the line are not finished
-		
+
 			x.push_back(string{token});	//put every word in string vector called x until the words in the line are finished	
 		}
-		
+
 		unsigned int s = stoul(x[1]);  //The word corrisponding to start coordinate converted from string to unsigned int
 		unsigned int e = stoul(x[2]);	//The word corrisponding to end coordinate converted from string to unsigned int
 
 
 		genomic_position new_class(x[0],s,e);  //modifiyng genomic_position class prova (inizialized as default before)
-						   //with the corrisponding data from the current file line
+		//with the corrisponding data from the current file line
 
 
 		if(new_class.get_flag() == 1){	//CONTROL: if flag is 1 means that the current line has starting coordinate > end coordinate, so it is correct
-			
+
 			centering_function(&new_class.start, &new_class.end, parameter); //function to center the coordinates
 			GEP.push_back(genomic_position{new_class});	//put the class prova in GAP (vector of classes of type genomic_position)
 		}
@@ -50,63 +50,66 @@ int main(int argc, char *argv[]){
 			cerr << "WARNING: the line " << n_line << " is omitted because starting coordinates > end coordinates, please check your BED file!" << "\n";
 			//if flag is not 1 means that the current line has starting coordinate < end coordinate: PRINT WARNING!		
 		}
-	
+
 		n_line = n_line + 1;			//pass to next line 
 	}
 
 
-	
+
 	for (int i=0; i<GEP.size(); ++i){    // from 0 to GEP vector length
 
 		cout<< GEP[i].chr << "\n" << GEP[i].start << "\n" << GEP[i].end << "\n" << GEP[i].get_flag() << "\n\n"; //control print
-		}
+	}
 
 }
 
 void centering_function (unsigned int *start, unsigned int *end, int p){
 
-		unsigned int overhead = 25;
-		unsigned int centro = (*start + *end)/2;
-		*start = centro - p -overhead;
-		*end = centro + p +overhead;
+	unsigned int overhead = 25;
+	unsigned int centro = (*start + *end)/2;
+	*start = centro - p -overhead;
+	*end = centro + p +overhead;
 
 }
 
 void command_line_parser(int argc, char **argv){
 
 
-for(int i = 1; i < argc; i++){
-	
-	string buf = argv[i];
+	for(int i = 1; i < argc; i++){
 
-	if(buf == "--help" || buf == "-h"){
+		string buf = argv[i];
 
-	display_help();
+		if(buf == "--help" || buf == "-h"){
 
-	}
+			display_help();
 
-	if(buf == "--BED" || buf == "-B"){
+		}
 
-		if(i < argc - 1){
-			
-			BED_FILE = argv[++i];
+		if(buf == "--BED" || buf == "-B"){
 
-		continue;
-        	}
-	}
-		
-	int is_parameter = stoi(buf);			//convert argument into integer and put in variable is_parameter
-	string input_type = typeid(is_parameter).name(); //put the type of is_parameter variable into a string called input_type
-	if(input_type == "i"){				//if input_type is "i" means that the current argument buf is an integer
+			if(i < argc - 1){
 
-			parameter = stoi(argv[i]);      //put the argument value (converted to integer) in parameter variable
-			
-	}
-	
+				BED_FILE = argv[++i];
+
+				continue;
+			}
+		}
+
+		if(buf == "--param" || buf == "-p"){
+
+			if(i < argc - 1){
+
+				parameter = stoi(argv[++i]);
+
+				continue;
+			}
+		}
+
+
 	}
 
 }
-	
+
 
 
 
@@ -115,7 +118,7 @@ void display_help()
 	cerr << "\n --help: show this message" << endl;
 	cerr << "\n --BED -B <file_bed>: input bed file" << endl;
 	cerr << endl;
-	
+
 	exit(EXIT_SUCCESS);
 }
 
