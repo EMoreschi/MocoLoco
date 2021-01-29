ccbdfea637942f925fe94697cc2a90cae347a568
EMoreschi <enrico.moreschi@studenti.unimi.it>
2021-01-29 10:31:33 +0000

    Changed main and constructor

--- MocoLoco.h
+++ MocoLoco.h
@@ -35,8 +35,30 @@ class genomic_position { //creation public class of genomic_position type
 			start_coord = 0;
 			end_coord = 0;
 			flag = 0;	
-		} 
+		
+		}
 
+		genomic_position(string chr, int start, int end, int p){
+		
+
+			flag = flag_control(start,end);
+			chr_coord = chr;
+
+			centering_function(&start, &end, p); //function to center the coordinates
+
+			start_coord = start;
+			end_coord = end;
+
+		}
+		
+		void centering_function ( int *start,  int *end, int p){
+	       
+			int overhead = 25;
+		       	int centro = (*start + *end)/2;
+			*start = centro - p -overhead;
+			*end = centro + p +overhead;
+		}
+		
 		bool flag_control( int st,  int en){ //function which controls that start coordinates are < then end coordinates
 
 			if(st > en){		//if start coordinates are > then end coordinates, flag is setted to 0
@@ -51,6 +73,6 @@ class genomic_position { //creation public class of genomic_position type
 };
 
 void GEP_objects_creation(const char*, const char*);
-void centering_function ( int*,  int*, int);
+void centering_function ( int*,  int*, int, int);
 void command_line_parser(int, char **);
 void display_help();
