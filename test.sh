c19a6e2797aeb04f2df59aad5dc27cca451fdd52
EMoreschi <enrico.moreschi@studenti.unimi.it>
2021-02-11 16:37:04 +0000

    bash for execution Mocoloco.cpp

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+g++ -o test MocoLoco.cpp;
+
+if [[ $2 == "clone" ]]; then
+
+	git clone https://github.com/EMoreschi/MocoLoco/tree/Test;
+fi
+
+Jaspar="JASPAR_MA/MA0002.2.jaspar JASPAR_MA/MA0042.2.jaspar JASPAR_MA/MA0071.1.jaspar JASPAR_MA/MA0093.3.jaspar"
+Twobit="hg38.2bit hg19.2bit"
+BED="baitedreagionsb38.bed nfy_hg19.bed"
+
+for a in $BED 
+do
+	for b in $Twobit 
+	do
+		for c in $Jaspar 
+		do
+			g++ -o test MocoLoco.cpp;
+			./test --BED ${a} -tb ${b} -J ${c} >> $1;
+			echo "----------------------------------------------------------------------------------------------------------------------------" >> $1;
+			
+		done
+	done
+done
