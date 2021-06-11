e4d32c60e52778e44d570c4540087436a113a1f4
EMoreschi <77453569+EMoreschi@users.noreply.github.com>
2021-06-11 11:44:01 +0200

    Update test_p_val.sh

--- testing/test_p_val.sh
+++ testing/test_p_val.sh
@@ -22,14 +22,14 @@ i=1;
 
 echo "IMPLANTING_FREQ	POSITION	RANK	OLIGO	OLIGO_FWD_OCC	OLIGO_REV_OCC	SUM_OLIGO_OCC	OLIGO_RC	OLIGO_RC_FWD_OCC	OLIGO_RC_REV_OCC	SUM_OLIGO_RC_OCC	PALINDROME	TOT_SUM	FREQ	P_VALUE" >> $Output;
 
+g++ -o random -Wall ../Random_multifa_TOOL/Multifa_random_tool.cpp;
+g++ -o mocoloco -Wall ../MocoLoco.cpp -lgsl -lgslcblas &&
+
 while [ $i -ne 101 ]
 do
 	
-	g++ -o test -Wall ../Random_multifa_TOOL/Multifa_random_tool.cpp;
-	./test -n $N_seq -l $Seq_length -j $Matrix -p $Position -o $i -w $Wobble -f $Frequence_FWD;
-
-	g++ -o test -Wall ../MocoLoco.cpp -lgsl -lgslcblas &&
-	./test -m random_multifa_implanted1.fasta -k $K;
+	./random -n $N_seq -l $Seq_length -j $Matrix -p $Position -o $i -w $Wobble -f $Frequence_FWD;
+	./mocoloco -m random_multifa_implanted1.fasta -k $K;
 	
 	touch $Output;
 	
