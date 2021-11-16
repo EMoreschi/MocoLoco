02bcaaf0e390f499f80b0c978dca23b6c23b3b22
amarettil <luca.amaretti@studenti.unimi.it>
2021-11-16 11:51:02 +0100

    Add Bonferroni correction

--- MocoLoco.cpp
+++ MocoLoco.cpp
@@ -1730,10 +1730,12 @@ double z_test_class::return_global_std_dev(){
 
 	return global_dev_std;
 }
+
 double z_test_class::return_Zpvalue(){
 
 	return Zpvalue;
 }
+
 double z_test_class::return_z_score(){
 
 	return z_score;
@@ -2329,7 +2331,7 @@ void map_class::print_debug_Z_scores(ofstream& outfile, unsigned int j, unsigned
 	outfile << "#Z_score parameters and p-value for hit positions - k = " << k << endl << endl;
 	string best_oligo;
 	
-	outfile << "#Position" << "\t" << "best_oligo" << "\t" << "Local_mean" << "\t" << "Global_mean" << "\t" << "Local_std_dev" << "\t" << "Global_std_dev" << "\t" << "Z_score" << "\t" << "P-value" << "\t" << "P-value_Log10" << endl;
+	outfile << "#Position" << "\t" << "best_oligo" << "\t" << "Local_mean" << "\t" << "Global_mean" << "\t" << "Local_std_dev" << "\t" << "Global_std_dev" << "\t" << "Z_score" << "\t" << "P-value" << "\t" << "P-value_Log10" << "\t" << "Bonferroni P-value" << "\t" << "Bonferroni_Log10" << endl;
 	
 	for(unsigned int position = 0; position < Z_TEST_MATRIX[j].size(); position++){
 	
@@ -2339,12 +2341,14 @@ void map_class::print_debug_Z_scores(ofstream& outfile, unsigned int j, unsigned
 		double local_dev_std = Z_TEST_MATRIX[j][position].return_local_std_dev();
 		double global_dev_std = Z_TEST_MATRIX[j][position].return_global_std_dev();
 		double Zpvalue  = Z_TEST_MATRIX[j][position].return_Zpvalue();
-		double Zpvalue_Log10  = log10(Z_TEST_MATRIX[j][position].return_Zpvalue())*-1;
+		double Zpvalue_Log10  = abs(log10(Z_TEST_MATRIX[j][position].return_Zpvalue()));
 		double z_score  = Z_TEST_MATRIX[j][position].return_z_score();
-	
+		double bonferroni = Zpvalue * ((half_length*2) - k);
+		double bonferroni_Log10 = abs(log10(bonferroni));
+
 		best_oligo = HAMMING_MATRIX[j][local_pos-1].return_real_best_oligo();	
 
-		outfile << local_pos << "\t" << best_oligo << "\t" << local_mean << "\t" << global_mean << "\t" << local_dev_std << "\t" << global_dev_std << "\t" << z_score << "\t" << Zpvalue << "\t" << Zpvalue_Log10 << endl;
+		outfile << local_pos << "\t" << best_oligo << "\t" << local_mean << "\t" << global_mean << "\t" << local_dev_std << "\t" << global_dev_std << "\t" << z_score << "\t" << Zpvalue << "\t" << Zpvalue_Log10 << "\t" << bonferroni << "\t" << bonferroni_Log10 << endl;
 	}
 }
 
