b9b9bf239e83ae9f8449e803a80528cb75191891
amarettil <luca.amaretti@studenti.unimi.it>
2022-06-06 18:01:40 +0200

    Resolve bug reverse complement in pvalue

--- MocoLoco.cpp
+++ MocoLoco.cpp
@@ -1274,8 +1274,7 @@ void MapClass::VerticalMapVector() {
            it != vector_map_ver[i].end(); it++) {
         if (it->second.vertical_count[j] > 0) {
           pos.emplace(it->second.vertical_count[j], it->first);
-        }
-        if (!it->second.palindrome && it->second.vertical_count_rc[j] > 0) {
+        } else if (!it->second.palindrome && it->second.vertical_count_rc[j] > 0) {
           pos.emplace(it->second.vertical_count_rc[j], it->second.oligo_rc);
         }
       }
